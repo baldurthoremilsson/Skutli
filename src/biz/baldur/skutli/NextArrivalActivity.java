738630b4ce9be8ad35fda826eedde93f933d09fc
Baldur Þór Emilsson <baldur@baldur.biz>
2011-12-11 13:40:05 +0000

    Removed redundant import

--- src/biz/baldur/skutli/NextArrivalActivity.java
+++ src/biz/baldur/skutli/NextArrivalActivity.java
@@ -14,7 +14,6 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.view.ViewGroup;
